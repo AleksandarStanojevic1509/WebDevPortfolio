ae059b4497b4ed4e56bbf020128694c3f68f1113
AleksandarStanojevic1509 <aleksandar.stanojevic.1509@gmail.com>
2020-06-20 21:49:49 +0200

    carousel

--- public/script.js
+++ public/script.js
@@ -21,8 +21,14 @@ const contactDropdownHendler = document.getElementById("contract-dropdown");
 const mainWrapper = document.getElementById('wrapper');
 const aboutBox = document.getElementById('about-me-box');
 const webSitesHandler = document.getElementById('web');
-const nextBtnHandler = document.getElementById('next');
-const prevBtnHandler = document.getElementById('prev');
+const gameHandler = document.getElementById('games');
+const appHandler = document.getElementById('app')
+const nextBtnHandlerWeb = document.getElementById('next-web');
+const prevBtnHandlerWeb = document.getElementById('prev-web');
+const nextBtnHandlerApp = document.getElementById('next-app');
+const prevBtnHandlerApp = document.getElementById('prev-app');
+const nextBtnHandlerGame = document.getElementById('next-game');
+const prevBtnHandlerGame = document.getElementById('prev-game');
 
 // console.log()
 
@@ -101,6 +107,27 @@ webSitesHandler.addEventListener("click", () => {
   document.getElementById("web-sites-box").style.animation = "backInDown 1s";
 });
 
+//  Game
+gameHandler.addEventListener("click", () => {
+  setTimeout(()=>{
+    mainWrapper.style.display = 'none';
+  },1010);
+  window.scrollTo(0, 0);
+  document.getElementById("game-box").style.display = "grid";
+  document.getElementById("game-box").style.animation = "backInDown 1s";
+});
+
+//  App
+appHandler.addEventListener("click", () => {
+  setTimeout(()=>{
+    mainWrapper.style.display = 'none';
+  },1010);
+  window.scrollTo(0, 0);
+  document.getElementById("app-box").style.display = "grid";
+  document.getElementById("app-box").style.animation = "backInDown 1s";
+});
+
+
 
 
 // close pages
@@ -115,6 +142,12 @@ returnHandler.forEach((elem) => {
     }
     if (document.getElementById("web-sites-box").style.display === "grid") {
       closePage(document.getElementById("web-sites-box"));
+    };
+    if (document.getElementById("game-box").style.display === "grid") {
+      closePage(document.getElementById("game-box"));
+    };
+    if (document.getElementById("app-box").style.display === "grid") {
+      closePage(document.getElementById("app-box"));
     };
   });
 });
@@ -131,11 +164,13 @@ document.addEventListener("scroll", (event) => {
   }
 });
 
+
+// WEB RENDER
 let slideCounter = 1;
 
-let displaySlide = (num) =>{
+let displaySlide = (num, shift) =>{
   let i;
-  let slides = document.getElementsByClassName("slide");
+  let slides = document.getElementsByClassName(shift);
   if (num > slides.length){
     slideCounter = 1;
   }
@@ -145,23 +180,165 @@ let displaySlide = (num) =>{
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
-  slides[slideCounter-1].style.display = "block"; 
+  slides[slideCounter-1].style.display = "block";
 }
 
-let shiftSlide = (num)=>{
-  displaySlide(slideCounter += num);
-}  
+let shiftSlide = (num, shift)=>{
+  displaySlide(slideCounter += num, shift);
+}
+
+const renderInfo = (title, firstEl, secondEl, thirdEl, gitRepo, live) =>{
+  return `<h1>${title}</h1>
+  <h3>Project info:</h3>
+  <ul>
+    <li>${firstEl}</li>
+    <li>${secondEl}</li>
+    <li>${thirdEl}</li>
+  </ul>
+  <div class="btn-box">
+    <a href="${live}" target="_blank">Live Version</a>
+    <button>View Project</button>
+    <a href="${gitRepo}" target="_blank">GitHub Repo</a>
+  </div>`
+}
+
+const resetAnimation = (id) =>{
+  setTimeout(()=>{
+    document.getElementById(id).style.animation = 'none'
+        }, 1001)
+}
+
+// web shifters
+const shiftPrevNextWeb = () =>{
+  if(slideCounter === 1){
+    nextBtnHandlerWeb.style.backgroundImage = `url("img/portfolio/ex-02.jpg")`;
+    prevBtnHandlerWeb.style.backgroundImage = `url("img/portfolio/ex-03.jpg")`;
+    document.getElementById('web-info').style.animation = 'fadeIn 1s'
+    document.getElementById('web-info').innerHTML = renderInfo(`GymFit`, `HTML, CSS, JS`, `Bootstrap 4`, `Responsive`, `https://github.com/AleksandarStanojevic1509/GymFit`, `#`)
+    resetAnimation('web-info')
+  }
+  if(slideCounter === 2){
+    nextBtnHandlerWeb.style.backgroundImage = `url("img/portfolio/ex-03.jpg")`;
+    prevBtnHandlerWeb.style.backgroundImage = `url("img/portfolio/gym-01.png")`;
+    document.getElementById('web-info').style.animation = 'fadeIn 1s'
+    document.getElementById('web-info').innerHTML = renderInfo(`the h<span><i class="fas fa-home"></i></span>use`, `HTML, CSS, JS`, `Bootstrap 4, Responsive`, `Firebase`, `https://github.com/AleksandarStanojevic1509/theHouse`, `#`)
+    resetAnimation('web-info')
+  }
+  if(slideCounter === 3){
+    nextBtnHandlerWeb.style.backgroundImage = `url("img/portfolio/gym-01.png")`;
+    prevBtnHandlerWeb.style.backgroundImage = `url("img/portfolio/ex-02.jpg")`;
+    document.getElementById('web-info').style.animation = 'fadeIn 1s'
+    document.getElementById('web-info').innerHTML = renderInfo(`Portfolio`,`HTML, CSS, JS`, `Bootstrap 4`, `Responsive`, `#`, `#`)
+    resetAnimation('web-info')
+  }
+}
+
+// game shifters
+const shiftPrevNextGame = () =>{
+  if(slideCounter === 1){
+    nextBtnHandlerGame.style.backgroundImage = `url("img/portfolio/ex-02.jpg")`;
+    prevBtnHandlerGame.style.backgroundImage = `url("img/portfolio/ex-04.jpg")`;
+    document.getElementById('game-info').style.animation = 'fadeIn 1s'
+    document.getElementById('game-info').innerHTML = renderInfo(`javaScript Quizz`, `HTML`, `CSS`, `JS`, `https://github.com/AleksandarStanojevic1509/JavaScript-Quizz`, `#`)
+    resetAnimation('game-info')
+  }
+  if(slideCounter === 2){
+    nextBtnHandlerGame.style.backgroundImage = `url("img/portfolio/ex-03.jpg")`;
+    prevBtnHandlerGame.style.backgroundImage = `url("img/portfolio/ex-01.jpg")`;
+    document.getElementById('game-info').style.animation = 'fadeIn 1s'
+    document.getElementById('game-info').innerHTML = renderInfo(`PaperRockScissors`, `HTML, CSS, JS`, `socket.io`, `node.js`, `https://github.com/AleksandarStanojevic1509/RockPaperScissors`, `#`)
+    resetAnimation('game-info')
+  }
+  if(slideCounter === 3){
+    nextBtnHandlerGame.style.backgroundImage = `url("img/portfolio/ex-04.jpg")`;
+    prevBtnHandlerGame.style.backgroundImage = `url("img/portfolio/ex-02.jpg")`;
+    document.getElementById('game-info').style.animation = 'fadeIn 1s'
+    document.getElementById('game-info').innerHTML = renderInfo(`Pig Game`,`HTML`, `CSS`, `JS`, `https://github.com/AleksandarStanojevic1509/PigGame`, `#`)
+    resetAnimation('game-info')
+  }
+  if(slideCounter === 4){
+    nextBtnHandlerGame.style.backgroundImage = `url("img/portfolio/ex-01.jpg")`;
+    prevBtnHandlerGame.style.backgroundImage = `url("img/portfolio/ex-03.jpg")`;
+    document.getElementById('game-info').style.animation = 'fadeIn 1s'
+    document.getElementById('game-info').innerHTML = renderInfo(`Zanimljiva Geografija`,`HTML, CSS, JS`, `Materialize, Responsive`, `socket.io, node.js`, `https://github.com/AleksandarStanojevic1509/ZanimljivaGeografija`, `https://zanimljivageografija-f1e43.firebaseapp.com/`)
+    resetAnimation('game-info')
+  }
+}
 
 
+// app shifters
+const shiftPrevNextApp = () =>{
+  if(slideCounter === 1){
+    nextBtnHandlerApp.style.backgroundImage = `url("img/portfolio/planner-01.png")`;
+    prevBtnHandlerApp.style.backgroundImage = `url("img/portfolio/cov-01.png")`;
+    document.getElementById('app-info').style.animation = 'fadeIn 1s'
+    document.getElementById('app-info').innerHTML = renderInfo(`Calculator`, `HTML`, `CSS`, `JS`, `https://github.com/AleksandarStanojevic1509/Calculator`, `#`)
+    resetAnimation('app-info')
+  }
+  if(slideCounter === 2){
+    nextBtnHandlerApp.style.backgroundImage = `url("img/portfolio/currency-01.png")`;
+    prevBtnHandlerApp.style.backgroundImage = `url("img/portfolio/calc-01.png")`;
+    document.getElementById('app-info').style.animation = 'fadeIn 1s'
+    document.getElementById('app-info').innerHTML = renderInfo(`Daily Planner`, `HTML, CSS, JS`, `Bootstrap 4, Responsive`, `indexedDB, dexie.js`, `https://github.com/AleksandarStanojevic1509/DailyPlanner`, `#`)
+    resetAnimation('app-info')
+  }
+  if(slideCounter === 3){
+    nextBtnHandlerApp.style.backgroundImage = `url("img/portfolio/wheater-01.png")`;
+    prevBtnHandlerApp.style.backgroundImage = `url("img/portfolio/planner-01.png")`;
+    document.getElementById('app-info').style.animation = 'fadeIn 1s'
+    document.getElementById('app-info').innerHTML = renderInfo(`Currency Calculator`,`HTML, CSS`, `JS`, `REST API`, `https://github.com/AleksandarStanojevic1509/CurrencyCalculator`, `#`)
+    resetAnimation('app-info')
+  }
+  if(slideCounter === 4){
+    nextBtnHandlerApp.style.backgroundImage = `url("img/portfolio/cov-01.png")`;
+    prevBtnHandlerApp.style.backgroundImage = `url("img/portfolio/currency-01.png")`;
+    document.getElementById('app-info').style.animation = 'fadeIn 1s'
+    document.getElementById('app-info').innerHTML = renderInfo(`Simple Weather App`,`HTML, CSS, JS`, `Bootstrap 4, Responsive`, `REST API`, `https://github.com/AleksandarStanojevic1509/SimpleWeatherApp`, `#`)
+    resetAnimation('app-info')
+  }
+  if(slideCounter === 5){
+    nextBtnHandlerApp.style.backgroundImage = `url("img/portfolio/calc-01.png")`;
+    prevBtnHandlerApp.style.backgroundImage = `url("img/portfolio/wheater-01.png")`;
+    document.getElementById('app-info').style.animation = 'fadeIn 1s'
+    document.getElementById('app-info').innerHTML = renderInfo(`Covid-19 Table`,`HTML, CSS, JS`, `Bootstrap 4, Responsive`, `REST API`,  `https://github.com/AleksandarStanojevic1509/Covid-19-table`, `#`)
+    resetAnimation('app-info')
+  }
+}
 
-
-nextBtnHandler.addEventListener('click', () =>{
-  shiftSlide(1);
+// web btns
+nextBtnHandlerWeb.addEventListener('click', () =>{
+  shiftSlide(1, 'web-shift');
+  shiftPrevNextWeb();
 })
 
-prevBtnHandler.addEventListener('click', () => {
-  shiftSlide(-1);
-} )
+prevBtnHandlerWeb.addEventListener('click', () => {
+  shiftSlide(-1, 'web-shift');
+  shiftPrevNextWeb();  
+})
+
+// app btn
+nextBtnHandlerApp.addEventListener('click', () =>{
+  shiftSlide(1, 'app-shift');
+  shiftPrevNextApp();
+})
+
+prevBtnHandlerApp.addEventListener('click', () => {
+  shiftSlide(-1, 'app-shift');
+  shiftPrevNextApp();  
+})
+
+// game btns
+nextBtnHandlerGame.addEventListener('click', () =>{
+  shiftSlide(1, 'game-shift');
+  shiftPrevNextGame();
+})
+
+prevBtnHandlerGame.addEventListener('click', () => {
+  shiftSlide(-1, 'game-shift');
+  shiftPrevNextGame();  
+})
+
+
 
 // const renderAboutBox = () =>{
 //   return `     <i class="fas fa-reply"></i>
